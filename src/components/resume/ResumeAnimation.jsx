5336e97cf7304b34064ddb95cb3d76519d14639d
andev1987 <antdev1987@gmail.com>
2021-12-20 02:20:38 -0600

    Runway Section finished

--- src/components/resume/ResumeAnimation.jsx
+++ src/components/resume/ResumeAnimation.jsx
@@ -1,63 +1,63 @@
-import React from "react";
-import Skills from "../skills/Skills";
+import React from 'react';
+import Skills from '../skills/Skills';
 
 const resumeContent = [
   {
-    jobPosition: `Sr. Front-end Engineer`,
-    jobType: `Google | Remote`,
-    jobDuration: `Feb 2021 - Present`,
-    timeDuraton: `Full Time`,
-    compnayName: "Google LLC",
-    jobDescription: `Adipisicing Lorem ipsum dolor sit amet, consectetur elit,
-    sed do eiusmod tempor incididunt ut labore et dolore magna
-    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    Duis aute irure dolor.`,
-    delayAnimation: "",
+    jobPosition: `2022 Q1 :`,
+    jobType: `Genesis Drop`,
+    list: [
+      { txt: 'Physical Items Redemption' },
+      { txt: 'Pop-up stores in 5 major cities' },
+      { txt: 'Artist line-up for new collection' },
+    ],
+    delayAnimation: '',
   },
   {
-    jobPosition: `Visual Designer`,
-    jobType: `Ridian | Remote`,
-    jobDuration: `Mar 2020 - Present`,
-    timeDuraton: `Part Time`,
-    compnayName: "Ridian IT",
-    jobDescription: `Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-    irure dolor. Lorem ipsum dolor sit amet, consectetur
-    adipisicing elit, sed do eiusmod tempor incididunt ut
-    labore et dolore magna aliqua.`,
-    delayAnimation: "100",
+    jobPosition: `2022 Q2 :`,
+    jobType: `Experiences`,
+    list: [
+      { txt: 'Fashion trunk shows' },
+      { txt: 'Metaverse fashion week 2022' },
+      { txt: 'Launch of pop-up stores in 10 major cities' },
+    ],
+    delayAnimation: '100',
   },
   {
-    jobPosition: `Animation Artist`,
-    jobType: `IbThemes | Remote`,
-    jobDuration: `Jan 2019 - Present`,
-    timeDuraton: `Full Time`,
-    compnayName: "IB Themes",
-    jobDescription: ` Consectetur adipisicing elit,lorem ipsum dolor sit amet,
-    sed do eiusmod tempor incididunt ut labore et dolore magna
-    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    Duis aute irure dolor.`,
-    delayAnimation: "200",
+    jobPosition: `2022 Q3 :`,
+    jobType: `Community`,
+    list: [
+      { txt: '$MIRL token launch' },
+      { txt: 'Launch of wear-to-earn e-commerce store' },
+      { txt: 'MIRL by invite app' },
+    ],
+    delayAnimation: '200',
+  },
+  {
+    jobPosition: `2022 Q4 :`,
+    jobType: `Domination`,
+    list: [
+      { txt: 'NFT holders owned fashion incubator' },
+      { txt: 'Voting portal for NFT holders to vote on collabs and designs' },
+    ],
+    delayAnimation: '300',
   },
 ];
 
 const educatonContent = [
   {
-    passingYear: "2018-2020",
-    degreeTitle: "Ph.D in Horriblensess",
-    instituteName: "University Of Evil Doing",
+    passingYear: '2018-2020',
+    degreeTitle: 'Ph.D in Horriblensess',
+    instituteName: 'University Of Evil Doing',
   },
   {
-    passingYear: "2013-2016",
-    degreeTitle: "Bsc. in Computer Science",
-    instituteName: "World University",
+    passingYear: '2013-2016',
+    degreeTitle: 'Bsc. in Computer Science',
+    instituteName: 'World University',
   },
   {
-    passingYear: "2010-2012",
-    degreeTitle: "Graphic Artist Training ",
-    instituteName: "Graphic Master Institute",
+    passingYear: '2010-2012',
+    degreeTitle: 'Graphic Artist Training ',
+    instituteName: 'Graphic Master Institute',
   },
 ];
 
@@ -66,8 +66,8 @@ const Resume = () => {
     <>
       <section id="resume" className="section">
         <div className="container">
-          <div className="title">
-            <h3>Experience.</h3>
+          <div className="title title-pink">
+            <h3>Runway</h3>
           </div>
           {/* End title */}
           <div className="resume-box">
@@ -77,21 +77,19 @@ const Resume = () => {
                 key={i}
                 data-aos="fade-up"
                 data-aos-duration="1200"
-                data-aos-delay={val.delayAnimation}
-              >
+                data-aos-delay={val.delayAnimation}>
                 <div className="row">
                   <div className="col-md-4 col-xl-3">
                     <div className="rb-left">
-                      <h6>{val.jobPosition}</h6>
-                      <label>{val.jobType}</label>
-                      <p>{val.jobDuration}</p>
-                      <div className="rb-time">{val.timeDuraton}</div>
+                      <h4>{val.jobPosition}</h4>
+                      <h6>{val.jobType}</h6>
                     </div>
                   </div>
                   <div className="col-md-8 col-xl-9">
                     <div className="rb-right">
-                      <h6>{val.compnayName}</h6>
-                      <p>{val.jobDescription}</p>
+                      {val.list.map((txt, idx) => (
+                        <p key={idx}>{txt.txt}</p>
+                      ))}
                     </div>
                   </div>
                 </div>
@@ -103,29 +101,22 @@ const Resume = () => {
           <div
             className="separated"
             style={{
-              backgroundImage: `url(${
-                process.env.PUBLIC_URL + "img/border-dark.png"
-              })`,
-            }}
-          ></div>
+              backgroundImage: `url(${process.env.PUBLIC_URL + 'img/border-dark.png'})`,
+            }}></div>
           {/* End separated */}
 
           <div className="title">
-            <h3>Education & Skills</h3>{" "}
+            <h3>Education & Skills</h3>{' '}
           </div>
 
           <div className="row align-items-center">
-            <div
-              className="col-lg-4 m-15px-tb"
-              data-aos="fade-up"
-              data-aos-duration="1200"
-            >
+            <div className="col-lg-4 m-15px-tb" data-aos="fade-up" data-aos-duration="1200">
               <ul className="aducation-box">
                 {educatonContent.map((val, i) => (
                   <li key={i}>
                     <span>{val.passingYear}</span>
                     <h6>{val.degreeTitle} </h6>
-                    <p>{val.instituteName}</p>{" "}
+                    <p>{val.instituteName}</p>{' '}
                   </li>
                 ))}
               </ul>
@@ -136,8 +127,7 @@ const Resume = () => {
               className="col-lg-7 ml-auto m-15px-tb"
               data-aos="fade-up"
               data-aos-duration="1200"
-              data-aos-delay="200"
-            >
+              data-aos-delay="200">
               <Skills />
             </div>
             {/* End .col */}
