6a0f2c08155032906d12705ab664f20a62c22a89
andev1987 <antdev1987@gmail.com>
2021-12-21 03:31:01 -0600

    removed

--- src/views/all-home-version/HomeDarkAnimation.jsx
+++ src/views/all-home-version/HomeDarkAnimation.jsx
@@ -31,7 +31,7 @@ const HomeOne = () => {
       <section id="gallery" className="section theme-light dark-bg">
         <div className="container">
           <div className="title">
-            <h3>My Gallery.</h3>
+            <h3>My Gallery</h3>
           </div>
           <Portfolio />
         </div>
