21c1ccdc13f5c91b80383cd3ab45ec33e5d112df
andev1987 <antdev1987@gmail.com>
2021-12-20 18:16:54 -0600

    background add Genesis

--- src/components/about/AboutAnimation.jsx
+++ src/components/about/AboutAnimation.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Testimonials from '../testimonial/TestimonialAnimation';
+import './About.scss';
 // import Services from '../service/ServiceAnimation';
 // import Awards from '../award/AwardsAnimation';
 
@@ -7,56 +8,58 @@ const About = () => {
   return (
     <>
       <section id="about" className="section theme-light dark-bg">
-        <div className="container">
-          <div className="row align-items-center justify-content-center">
-            <div className="col-md-6 col-lg-4" data-aos="fade-up" data-aos-duration="1200">
-              <div className="about-me">
-                <div className="img">
-                  <div className="img-in">
-                    <img src="img/about/MIRL.jpg" alt="about" />
+        <div className="flexcenter aboutSection" style={{ background: 'url(./img/world.png) right center / cover' }}>
+          <div className="container">
+            <div className="row align-items-center justify-content-center">
+              <div className="col-md-6 col-lg-4" data-aos="fade-up" data-aos-duration="1200">
+                <div className="about-me">
+                  <div className="img">
+                    <div className="img-in">
+                      <img src="img/about/MIRL.jpg" alt="about" />
+                    </div>
+                  </div>
+                  {/* End img */}
+                </div>
+                {/* End about-me */}
+              </div>
+              {/* End col */}
+              <div className="col-lg-7 ml-auto" data-aos="fade-up" data-aos-duration="1200" data-aos-delay="200">
+                <div className="about-info">
+                  <div className="title">
+                    <h3>Genesis MIRL - S/S 2022</h3>
+                  </div>
+                  <div className="about-text">
+                    <p>
+                      MIRL™ pioneers the FashionFi movement that infuses the fashion industry with the advent of NFTs
+                      and DeFi.
+                    </p>
+                    <p>
+                      We are a brand with an ethos of the future, representing a new world culture that connects
+                      like-minded individuals.
+                    </p>
+                    <p>
+                      All MIRL™ will unlock exclusive access to seasonal physical wearables and privileges through our
+                      IRL partners.
+                    </p>
+                    <p>We want you to represent your digital presence, IRL.</p>
                   </div>
                 </div>
-                {/* End img */}
               </div>
-              {/* End about-me */}
+              {/* End col */}
             </div>
-            {/* End col */}
-
-            <div className="col-lg-7 ml-auto" data-aos="fade-up" data-aos-duration="1200" data-aos-delay="200">
-              <div className="about-info">
-                <div className="title">
-                  <h3>Genesis MIRL - S/S 2022</h3>
-                </div>
-                <div className="about-text">
-                  <p>
-                    MIRL™ pioneers the FashionFi movement that infuses the fashion industry with the advent of NFTs and
-                    DeFi.
-                  </p>
-                  <p>
-                    We are a brand with an ethos of the future, representing a new world culture that connects
-                    like-minded individuals.
-                  </p>
-                  <p>
-                    All MIRL™ will unlock exclusive access to seasonal physical wearables and privileges through our IRL
-                    partners.
-                  </p>
-                  <p>We want you to represent your digital presence, IRL.</p>
-                </div>
-              </div>
-            </div>
-            {/* End col */}
           </div>
+        </div>
 
-          {/* separated */}
+        {/* separated */}
 
-          <div
-            className="separated"
-            style={{
-              backgroundImage: `url(${process.env.PUBLIC_URL + 'img/border-dark.png'})`,
-            }}></div>
+        <div
+          className="separated"
+          style={{
+            backgroundImage: `url(${process.env.PUBLIC_URL + 'img/border-dark.png'})`,
+          }}></div>
 
-          {/* End separated */}
-
+        {/* End separated */}
+        <div className="container">
           {/* <div className="title">
             <h3>What I do?</h3>
           </div>
