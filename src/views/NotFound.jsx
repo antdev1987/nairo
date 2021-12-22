a4fe4ad259bdf73bed6821d701823e0232a3103e
andev1987 <antdev1987@gmail.com>
2021-12-21 22:44:22 -0600

    deleted some versions

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Footer from "../components/footer/Footer";
-import useDocumentTitle from "../components/useDocumentTitle";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from '../components/footer/Footer';
+import useDocumentTitle from '../components/useDocumentTitle';
 
 const NotFound = () => {
-  useDocumentTitle("Not Found || React Personal Portfolio Template");
+  useDocumentTitle('Not Found || React Personal Portfolio Template');
   return (
     <>
       <section className="error-page-wrapper">
@@ -12,12 +12,8 @@ const NotFound = () => {
           <div className="row justify-content-center full-screen align-items-center">
             <div className="col-lg-8 text-center">
               <div className="inner">
-                <h1 className="display-3 white-color m-15px-b">
-                  404 - Page Not Found..
-                </h1>
-                <p className="h4">
-                  Whoops, it looks like the page you request wasn't found.
-                </p>
+                <h1 className="display-3 white-color m-15px-b">404 - Page Not Found..</h1>
+                <p className="h4">Whoops, it looks like the page you request wasn't found.</p>
                 <div className="btn-bar mt-4">
                   <Link className="px-btn px-btn-white" to="/">
                     BACK TO HOME
